f2a7245ef7bae9c47d89d9f61fabaadc56d5ebbe
zain148 <zainarshad028@gmail.com>
2020-05-29 09:16:06 +0500

    Api setup

---
+++ index.js
@@ -0,0 +1,69 @@
+const stripe = require("stripe")("sk_test_ThlDssrPmTUhR9DGYEf0A4Le00LbNvGZCO");
+//set for local environment request
+const express = require("express");
+const cors = require("cors");
+
+//const ngrok = require("ngrok");
+/*****mAIN THING ABOUT CONNECTING APP WITH LOCAL HOST
+ * WE NEED ngrok server that mocks over localhost to
+ * https like this
+ * we install it using
+ * npm install ngrok -g
+ * then type ngrok http 8000
+ */
+//const cors = require("cors")({ origin: true });
+const uuid = require("uuid/v4");
+const app = express();
+//Middleware
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+app.use(express.json());
+const idempontencyKey = uuid();
+
+app.post("/Payment", (request, response) => {
+  const { amount, currency, token } = request.body;
+
+  // eslint-disable-next-line promise/catch-or-return
+
+  stripe.charges
+    .create(
+      {
+        amount: amount,
+        currency: currency,
+        source: token.id,
+        description: "Transaction for Lawn Ninja Pay In Advance",
+      },
+      {
+        idempotencyKey: idempontencyKey,
+      },
+      // this will handle error in stripe and show in console the return message
+      function (err, charge) {
+        console.log("error", err);
+        //this return charge will return the success object
+        console.log("Done charge", charge);
+      }
+    )
+    // eslint-disable-next-line promise/always-return
+    .then((charge) => {
+      response.send(charge);
+      // console.log("I'm calling Data outside",Data)
+    })
+    //this will return status to our frontend with status 200 when it's done
+    .then((result) => {
+      return response.status(200).json(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  //closing of post request
+});
+
+//to show some message on get request
+app.get("/", (req, res) => {
+  res.send("It Working Smoothly on heroku server zain!");
+});
+
+//will start hosting like localhost:8282 you'll see message in get
+app.listen(8000, () => console.log("Port is RUnning"));
+//exports.Payment = functions.https.onRequest(app);
