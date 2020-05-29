16a85749b070e64732cb506424272b344a3c3d42
zain148 <zainarshad028@gmail.com>
2020-05-29 11:23:25 +0500

    done

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ app.get("/Payment", (request, response) => {
       function (err, charge) {
         console.log("error", err);
         //this return charge will return the success object
+        alert(charge);
         //  console.log("Done charge", charge);
       }
     )
